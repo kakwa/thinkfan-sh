fb0a86a198193e928ee1ac5c84aef50c31bc1644
Carpentier Pierre-Francois <carpentier.pf@gmail.com>
2013-01-08 15:08:53 -0500

    Update thinkfan.sh

--- thinkfan.sh
+++ thinkfan.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
+#fan speed for the different states
+STATE_1_LEVEL=1
+STATE_2_LEVEL=5
+STATE_3_LEVEL=7
+
+#Limit temperature when we switch from 1 to 2 while going up
 LIMIT_1_UP=65
+#Limit temperature when we switch from 2 to 1 while going down
 LIMIT_1_DOWN=60
+
+#Limit temperature when we switch from 2 to 3 while going up
 LIMIT_2_UP=75
+#Limit temperature when we switch from 2 to 1 while going down
 LIMIT_2_DOWN=65
 
 CURRENT_LIMIT_UP=$LIMIT_1_UP
@@ -24,14 +34,13 @@ elif [ $MAX_TEMP -lt $LIMIT_2_DOWN ] && [ $STATE -eq 3 ]
 	then STATE=2
 fi
 case $STATE in
-	1) echo level 1 >/proc/acpi/ibm/fan;;
-	2) echo level 5 >/proc/acpi/ibm/fan;;
-	3) echo level 7 >/proc/acpi/ibm/fan;;	
+	1) echo level $STATE_1_LEVEL >/proc/acpi/ibm/fan;;
+	2) echo level $STATE_2_LEVEL >/proc/acpi/ibm/fan;;
+	3) echo level $STATE_3_LEVEL >/proc/acpi/ibm/fan;;	
 	:) echo level 7 >/proc/acpi/ibm/fan;;
 esac	
 }
 
-get_max_temperature
 while [ 1 ]
 do
 	get_max_temperature
